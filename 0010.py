a2bebd3b40d3a37183d69082306d5a3fab76b250
Brightj <523711448@qq.com>
2018-03-03 11:23:13 +0800

    Update 0010.py

--- 0010.py
+++ 0010.py
@@ -2,35 +2,32 @@ from PIL import Image, ImageDraw, ImageFont, ImageFilter
 import random
 
 
-def rndChar()
+def rndChar():
     return chr(random.randint(65, 90))
 
 
-def rndColor()
+def rndColor():
     return(random.randint(64,255),random.randint(64,255),random.randint(64,255))
 
 
-def rndColor2()
+def rndColor2():
     return(random.randint(32,127),random.randint(32,127),random.randint(32,127))
 
 
-def verCode()
-    width = 60  4
+def verCode():
+    width = 60 * 4
     height = 60
     image = Image.new('RGB', (width, height), (255, 255, 255))
-    font = ImageFont.truetype(r'cwindowsfontsArial.ttf', 36)
+    font = ImageFont.truetype(r'c:\windows\fonts\Arial.ttf', 36)
     draw = ImageDraw.Draw(image)
-    for x in range(width)
-        for y in range(height)
+    for x in range(width):
+        for y in range(height):
             draw.point((x, y), fill=rndColor())
-    for t in range(4)
-        draw.text((60  t + 10, 10), rndChar(), font=font, fill=rndColor2())
+    for t in range(4):
+        draw.text((60 * t + 10, 10), rndChar(), font=font, fill=rndColor2())
     image = image.filter(ImageFilter.BLUR)
-    image.save('dgakkicode.jpg', 'jpeg')
+    image.save('d:\gakki\code.jpg', 'jpeg')
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     verCode()
-
-
-
